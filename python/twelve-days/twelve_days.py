3e138672bcd47e2f1ba61fe38c40badfd40ce949
hritikb <79982592+hritikb@users.noreply.github.com>
2021-08-15 18:30:37 +0530

    Improved twelve_days.py

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,16 +1,17 @@
 def recite(start_verse, end_verse):
-    lyrics = ['On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.', 
-    'On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.',
-    'On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.', 
-    'On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.', 
-    'On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.', 
-    'On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.', 
-    'On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.', 
-    'On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.', 
-    'On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.', 
-    'On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.', 
-    'On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.', 
-    'On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.']
 
-    return lyrics[start_verse-1 : end_verse]
+    Dict = dict([(1, 'first'), (2, 'second'), (3, 'third'), (4, 'fourth'), (5, 'fifth'), (6, 'sixth'),
+    (7, 'seventh'), (8, 'eighth'), (9, 'ninth'), (10, 'tenth'), (11, 'eleventh'), (12, 'twelfth')])
+    
+   
+
+    lyrics = [' twelve Drummers Drumming,'
+    , ' eleven Pipers Piping,', ' ten Lords-a-Leaping,', ' nine Ladies Dancing,', ' eight Maids-a-Milking,', ' seven Swans-a-Swimming,', ' six Geese-a-Laying,', ' five Gold Rings,', ' four Calling Birds,', ' three French Hens,', ' two Turtle Doves, and', ' a Partridge in a Pear Tree.']
+    
+    out = []
+
+    for i in range(start_verse, end_verse + 1):
+        out.append('On the ' + Dict[i] + ' day of Christmas my true love gave to me:' + ''.join(lyrics[-i : ]))
+
+    return out
     
